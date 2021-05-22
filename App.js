3c86e47ff53396f61e3a2093c92a1170dc8feb0b
bmurdock <brian.j.murdock@gmail.com>
2021-05-21 18:24:26 -0600

    example

--- App.js
+++ App.js
@@ -1,21 +1,99 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, AsyncStorage } from 'react-native';
+import {fetchJSON, articlesRoute, sourcesRoute} from './config';
+import headlines from './data/headlines';
+
+// 67752668abc440628948a2c544e6cfb9
+
+// expo init foldername
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.toolbar}>
+        <View style={styles.toolBarThird}>
+          <Text>First</Text>
+        </View>
+        <View style={[styles.toolBarThird, styles.toolBarIcons]}>
+          <TinyIcon title="Home" />
+          <TinyIcon title="Meetings" />
+          <TinyIcon title="Contacts" />
+        </View>
+        <View style={[styles.toolBarThird, {textAlign: 'right'}]}>
+          <Text>Third</Text>
+        </View>
+      </View>
+      <View style={styles.contentArea}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const TinyIcon = (props) => {
+  return (
+    <View style={styles.tinyIcon}>
+      <Image 
+        style={styles.tinyIconImage}
+        source={{
+          uri: 'https://reactnative.dev/img/header_logo.svg',
+        }}
+      />
+      <Text>{props.title}</Text>
+    </View>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#333',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    display: 'flex',
+    flexDirection: 'column',
+  
   },
+  toolbar: {
+    backgroundColor: '#555',
+    width: '100%',
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  contentArea: {
+    flex: 9,
+  },
+  toolBarThird: {
+    flex: 1,
+    borderWidth: 1,
+  },
+  toolBarIcons: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  red: {
+    color: 'red',
+  },
+  blue: {
+    color: 'blue',
+  },
+  green: {
+    color: 'pink',
+  },
+  tinyIcon: {
+    display: 'flex',
+    flexDirection: 'column',
+    height: '100%',
+    flex: 1,
+    alignItems: 'center',
+  },
+  tinyIconImage: {
+    width: 25,
+    height: 25,
+  }
 });
---
+++ config.js
@@ -0,0 +1,27 @@
+export const apiKey = `67752668abc440628948a2c544e6cfb9`;
+export const baseRoute = `https://newsapi.org/v2`;
+export const sourceRoute = `${baseRoute}/sources?apiKey=${apiKey}&language=en&country=us`
+export const articlesRoute = `${baseRoute}/top-headlines?country=us&apiKey=${apiKey}`;
+
+export const fetchJSON = async (route) =>
+{
+    if (typeof route !== 'string' && route !== '') {
+        return new Error("Route must be a string.");
+    }
+    // validate url format? that would be complex
+    return await fetch(route)
+    .then((response) => 
+    {
+        if (response.status === 200) {
+            try {
+                return response.json();
+            }
+            catch (err) {
+                console.error('Could not return json: ', err);
+                return err;
+            }
+        }
+        return new Error('Failed response: ', response);
+    })
+}
+
